bf2c28c2abcde63f61ed68782f69e367db8954ed
Jeongjiw00 <wldn0229@naver.com>
2023-11-01 04:37:48 +0900

    [추가] project 모듈에 엔티티 가져오기

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ProjectsController } from './projects.controller';
 import { ProjectsService } from './projects.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Projects } from 'src/entities/projects.entity';
+import { Users } from 'src/entities/users.entity';
+import { Comments } from 'src/entities/comments.entity';
+import { Likes } from 'src/entities/likes.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Projects, Users, Comments, Likes])],
   controllers: [ProjectsController],
-  providers: [ProjectsService]
+  providers: [ProjectsService],
 })
 export class ProjectsModule {}
