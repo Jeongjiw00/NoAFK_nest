7cc843252ef9c92cee513cbf219f417fe464eee2
Jeongjiw00 <wldn0229@naver.com>
2023-11-09 21:24:55 +0900

    [추가] dummy 데이터 추가

---
+++ test/dummy/comment.dummy.ts
@@ -0,0 +1,29 @@
+export const CommentDummy = [
+  {
+    commentId: 1,
+    comment: 'test1',
+    projectId: 1,
+    userId: 1,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+  },
+  {
+    commentId: 2,
+    comment: 'test2',
+    projectId: 2,
+    userId: 2,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+  },
+  {
+    commentId: 3,
+    comment: 'test3',
+    projectId: 3,
+    userId: 3,
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    deletedAt: null,
+  },
+];
---
+++ test/dummy/like.dummy.ts
@@ -0,0 +1,14 @@
+export const LikeDummy = [
+  {
+    articleId: 1,
+    userId: 1,
+  },
+  {
+    articleId: 2,
+    userId: 2,
+  },
+  {
+    articleId: 3,
+    userId: 3,
+  },
+];
