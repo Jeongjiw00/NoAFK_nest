8359b0c5bd87d4d4b6e281b5055b1c29f037087b
Jeongjiw00 <wldn0229@naver.com>
2023-11-30 23:09:21 +0900

    [수정] optional 수정

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -3,22 +3,22 @@ import { IsEmail, IsOptional, IsString, Matches } from 'class-validator';
 export class UpdateUserDto {
   @IsOptional()
   @IsEmail({}, { message: 'email' })
-  email?: string;
+  email: string;
 
   @IsOptional()
   @Matches(
     /^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/,
     { message: 'newPassword' },
   )
-  password?: string;
+  password: string;
 
   @IsOptional()
   @Matches(/^[\u1100-\u11FF\u3130-\u318F\uAC00-\uD7AFa-z\d]{2,15}$/, {
     message: 'nickname',
   })
-  nickname?: string;
+  nickname: string;
 
   @IsOptional()
   @IsString({ message: 'introduction' })
-  introduction?: string;
+  introduction: string;
 }
